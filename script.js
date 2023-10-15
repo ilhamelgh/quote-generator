dd81306c8f9d88f72dca31963c450d7a6e0bb059
ilhamelgharbaoui <ielgharbaoui@azura-group.com>
2023-10-15 12:55:36 +0100

    code refactoring

--- script.js
+++ script.js
@@ -7,21 +7,19 @@ const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-// Show loading
-function loading() {
+function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
+function removeLoadingSpinner() {
     quoteContainer.hidden = false;
     loader.hidden = true;
 }
 
 // Show New Quote
 function newQuote() {
-    loading();
+    showLoadingSpinner();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -37,15 +35,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
-
-
     // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
-    complete();
+    removeLoadingSpinner();
+    
 }
 // Get Quotes From API
 async function getQuotes() {
-    loading();
+    showLoadingSpinner();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
@@ -53,7 +50,7 @@ async function getQuotes() {
         newQuote();
     } catch (error) {
         alert(error);
-        // Catch Error Here
+        getQuotes();
     }
 }
 
